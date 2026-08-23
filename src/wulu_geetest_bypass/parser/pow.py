0f0b720348e26acdaba6120a0f856e76f1c78d3a
wulu007 <dabaobeix@gmail.com>
2026-08-23 23:22:47 +0800

    refactor(pow): consolidate hash function mapping and thresholds

--- src/wulu_geetest_bypass/parser/pow.py
+++ src/wulu_geetest_bypass/parser/pow.py
@@ -1,6 +1,13 @@
 import hashlib
 import uuid
 
+_THRESHOLDS = {1: '7', 2: '3', 3: '1'}
+_HASH_FN = {
+    'md5': hashlib.md5,
+    'sha1': hashlib.sha1,
+    'sha256': hashlib.sha256,
+}
+
 
 def generate_pow(
     lot_number: str,
@@ -12,16 +19,15 @@ def generate_pow(
     datetime: str = '',
     extra: str = '',
 ) -> dict:
-    if hashfunc not in hash_mapping:
+    if hashfunc not in _HASH_FN:
         raise ValueError(f'Unsupported hash algorithm: {hashfunc}')
-    hasher_creator = hash_mapping[hashfunc]
+    hasher_creator = _HASH_FN[hashfunc]
 
     remainder = bits % 4
     zero_count = bits // 4
     prefix = '0' * zero_count
 
-    thresholds = {1: '7', 2: '3', 3: '1'}
-    threshold = thresholds.get(remainder)
+    threshold = _THRESHOLDS[remainder]
 
     header = (
         f'{version}|{bits}|{hashfunc}|{datetime}|{captcha_id}|{lot_number}|{extra}|'
@@ -39,10 +45,3 @@ def generate_pow(
             return {'pow_msg': message, 'pow_sign': h}
         elif h[zero_count] <= threshold:
             return {'pow_msg': message, 'pow_sign': h}
-
-
-hash_mapping = {
-    'md5': hashlib.md5,
-    'sha1': hashlib.sha1,
-    'sha256': hashlib.sha256,
-}
